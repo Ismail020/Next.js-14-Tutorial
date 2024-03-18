5105bc5ced9ce3c7f737b6eb820e93efe0160ca2
Ismail Çetin <isocetin1907@gmail.com>
2024-03-18 11:20:29 +0100

    Conditional Routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -5,13 +5,16 @@ export default function DashboardLayout({
     users,
     revenue,
     notifications,
+    login
 }: {
     children: React.ReactNode;
     users: React.ReactNode;
     revenue: React.ReactNode;
     notifications: React.ReactNode;
+    login: React.ReactNode;
 }) {
-    return (
+    const isLoggedIn = false;
+    return isLoggedIn ? (
         <div>
             <div>{children}</div>
             <div style={{ display: "flex" }}>
@@ -22,5 +25,7 @@ export default function DashboardLayout({
                 <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
             </div>
         </div>
+    ) : (
+        <div>{login}</div>
     );
 }
